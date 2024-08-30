5c87ddb7f13630e36d920226b9d5450987dda4cd
Johantandukar <johantandukar10@gmail.com>
2024-08-30 20:24:09 +0545

    new design

--- js/main.js
+++ js/main.js
@@ -313,7 +313,7 @@
 
 	function makeTimer() {
 
-		var endTime = new Date("31 August 2024 9:56:00 GMT+01:00");			
+		var endTime = new Date("31 September 2024 9:56:00 GMT+01:00");			
 		endTime = (Date.parse(endTime) / 1000);
 
 		var now = new Date();
